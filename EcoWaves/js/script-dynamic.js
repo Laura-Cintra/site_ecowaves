3ff7b21fbcb0c693d07f233e2d3d06fd8630713f
Vinicius Saes <viniciusaesouza@gmail.com>
2024-06-04 18:54:26 -0300

    update atividades

--- EcoWaves/js/script-dynamic.js
+++ EcoWaves/js/script-dynamic.js
@@ -10,7 +10,7 @@ if (opcao == "1") {
   document.querySelector("#img-principal").src =
     "../img/ongs/ondas-do-mar 1.png";
   document.querySelector("#pr1").innerHTML = "fazer faxina";
-  document.querySelector("#hr1").innerHTML = "14hrs as 12hrs";
+  document.querySelector("#hr1").innerHTML = "07hrs às 10hrs";
   document.querySelector("#pr2").innerHTML = " ";
   document.querySelector("#hr2").innerHTML = " ";
   document.querySelector("#pr3").innerHTML = " ";
@@ -31,7 +31,7 @@ if (opcao == "1") {
     "Rua Osmar Goncalves Nina, 49 JARDIM BOPIRANGA ITANHAEM - SP";
   document.querySelector("#img-principal").src = "../img/ongs/ondas-do-mar 1.png";
   document.querySelector("#pr1").innerHTML = "fazer faxina";
-  document.querySelector("#hr1").innerHTML = "07h às 10h";
+  document.querySelector("#hr1").innerHTML = "08h às 10h";
   document.querySelector("#pr2").innerHTML = "";
   document.querySelector("#hr2").innerHTML = " ";
   document.querySelector("#pr3").innerHTML = " ";
@@ -40,10 +40,10 @@ if (opcao == "1") {
   document.querySelector("#hr4").innerHTML = "";
   document.querySelector("#descricao").innerHTML =
     "O programa de Voluntariado Ecosurf possibilita a todos participantes a oportunidade de aprender na prática os valores da cidadania ambiental, meios para atuação em rede, cyberativismo, promoção de ações direta e mobilização. Através de uma grande rede de voluntários é possível potencializar e democratizar práticas que encorajam as pessoas na mudança de hábitos e atitudes para a proteção das praias e oceano.";
-  document.querySelector("#parc1").src = "../img/ongs/ondas-do-mar 1.png";
-  document.querySelector("#parc2").src = "../img/ongs/ondas-do-mar 1.png";
-  document.querySelector("#parc3").src = "../img/atividade";
-  document.querySelector("#logo-ong").src = "../img/atividade/ecosurf-logo.png";
+  document.querySelector("#parc1").src = "../img/atividade/pousada1.png";
+  document.querySelector("#parc2").src = "../img/atividade/pousada2.png";
+  document.querySelector("#parc3").src = "../img/atividade/pousada3.png";
+  document.querySelector("#logo-ong").src = "../img/atividade/ecosurf-logo.jpeg";
   document.querySelector("#nome-ong").innerHTML = "EcoSurf";
-  document.querySelector("#descricao-ong").innerHTML = "dsadasdasdasdas";
+  document.querySelector("#descricao-ong").innerHTML = "O programa de Voluntariado Ecosurf possibilita a todos participantes a oportunidade de aprender na prática os valores da cidadania ambiental, meios para atuação em rede, cyberativismo, promoção de ações direta e mobilização. Através de uma grande rede de voluntários é possível potencializar e democratizar práticas que encorajam as pessoas na mudança de hábitos e atitudes para a proteção das praias e oceano.";
 }
